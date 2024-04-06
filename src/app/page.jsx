8d47576e4cd2d53680a96ced528a6270b0ca4786
Sumit Kumar <sumitkumar@kgpian.iitkgp.ac.in>
2024-04-06 11:45:37 +0530

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,7 @@ const Homepage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          
           <Image src="/hero.png" alt="" fill className="object-contain" />
         </div>
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
