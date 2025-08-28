1b87266242b5082a72ab12a780cc04b296d6994b
Kartik Bhatnagar <kartik@trumio.ai>
2025-08-29 03:49:13 +0530

    fix

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -11,6 +11,34 @@ const TeamPage = () => {
       contactNumber: "+91 8829095000"
     },
     {
+      name: "Tushar Kumar",
+      image: "tushar.JPG",
+      linkedinUrl: "https://www.linkedin.com/in/tushar-kumar-33a932146/",
+      instagramUrl: "https://www.instagram.com/_snapshot_oo7?igsh=MTloa2s3N2UwbmNtaQ%3D%3D&utm_source=qr ",
+      contactNumber: "+91 6265983819"
+    },
+    {
+      name:"Hari Prasad Gardas",
+      image:"hari.jpg",
+      linkedinUrl:"https://www.linkedin.com/in/hari-prasad-gardas-618536232",
+      instagramUrl:"https://www.instagram.com/hari_gardas?igsh=b3owbDU4dDhwaHhp",
+      contactNumber:"+91 7416767210"
+    },
+    {
+      name:"Rakesh Rulaniya",
+      image:"rakesh.jpg",
+      linkedinUrl:"https://www.linkedin.com/in/rakesh-rulaniya-958a2222b/",
+      instagramUrl:"https://www.instagram.com/rakeshr2131?igsh=MXJycGx2Y3B2cms4ag==",
+      contactNumber:"+91 8690594016"
+    },
+    {
+      name:"Kartik Bhatnagar",
+      image:"kartik.jpeg",
+      linkedinUrl:"https://www.linkedin.com/in/kartik-bhatnagar-b2b042230/",
+      instagramUrl:"https://www.instagram.com/bhatnagarkartik/",
+      contactNumber:"+91 8875479888"
+    },
+    {
       name: "Shameek Saha",
       image: "shameek_saha.jpg",
       linkedinUrl: "https://www.linkedin.com/in/shameek-saha-5b0838287?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
@@ -24,34 +52,6 @@ const TeamPage = () => {
       instagramUrl: "https://www.instagram.com/divyanshu_baurai?igsh=anU4cnhtOHVzY280",
       contactNumber: "+91 6394752933"
     },
-    {
-      name: "Tushar Kumar",
-      image: "tushar.JPG",
-      linkedinUrl: "https://www.linkedin.com/in/tushar-kumar-33a932146/",
-      instagramUrl: "https://www.instagram.com/_snapshot_oo7?igsh=MTloa2s3N2UwbmNtaQ%3D%3D&utm_source=qr ",
-      contactNumber: "+91 6265983819"
-    },
-    {
-      name:"Hari Prasad Gardas",
-      image:"hari.JPG",
-      linkedinUrl:"https://www.linkedin.com/in/hari-prasad-gardas-618536232",
-      instagramUrl:"https://www.instagram.com/hari_gardas?igsh=b3owbDU4dDhwaHhp",
-      contactNumber:"+91 7416767210"
-    },
-    {
-      name:"Kartik Bhatnagar",
-      image:"kartik.jpeg",
-      linkedinUrl:"https://www.linkedin.com/in/kartik-bhatnagar-b2b042230/",
-      instagramUrl:"https://www.instagram.com/bhatnagarkartik/",
-      contactNumber:"+91 8875479888"
-    },
-    {
-      name:"Rakesh Rulaniya",
-      image:"rakesh.jpg",
-      linkedinUrl:"https://www.linkedin.com/in/rakesh-rulaniya-958a2222b/",
-      instagramUrl:"https://www.instagram.com/rakeshr2131?igsh=MXJycGx2Y3B2cms4ag==",
-      contactNumber:"+91 8690594016"
-    }
   ];
 
   return (
